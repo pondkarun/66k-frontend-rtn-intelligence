83734c019dbf4bda9abe841214511a85f63624f1
pondkarun <pondkarun@gmail.com>
2023-05-31 23:31:06 +0700

    setting pages

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const MyDocument = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="shortcut icon" href="next.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
