0cb4b53e77d3140e1983a44ed132144e3bb94861
pondkarun <52126943+pondkarun@users.noreply.github.com>
2023-07-30 21:24:14 +0700

    IONS Working Groups

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,7 +1,7 @@
-import { Badge, Col, Layout, Menu, Row, Space } from 'antd';
+import { Badge, Col, Layout, Menu, Modal, Row, Space } from 'antd';
 import { useSelector } from 'react-redux';
 import type { MenuProps } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from "styled-components";
 import { primary_color } from '@/pages/_app';
 const { Sider } = Layout;
@@ -11,7 +11,6 @@ const H1 = styled("h1")`
 `
 const ColText = styled(Col)`
    color: ${primary_color};
-//    text-align: center;
 `
 
 const Sidebar = styled(Sider)`
@@ -82,10 +81,48 @@ const ColMenuFlag = styled(Col)`
     padding-bottom: 5px;
 `
 
+const ModalIWG = styled(Modal)`
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        th {
+            border: 1px solid #dddddd;  
+        }
+        th:nth-child(1) {
+            width: 60%;
+        }
+        th:nth-child(2) {
+            width: 40%;
+        }
+    } 
+`
+
+const FlagIWG = styled("div")`
+    text-align: center;
+    padding: 15px;
+    &:hover {
+        background: ${primary_color};
+        .text {
+            color: #fff;
+            font-size: 18px;
+        }
+    }
+    img {
+        width: 80px;
+    }
+    .text {
+        font-size: 16px;
+    }
+`
+const FlagIWGGroup = styled("div")`
+    display: flex;
+    flex-wrap: wrap;
+`
 
 type MenuItem = Required<MenuProps>['items'][number];
 const SidebarLayoutComponents = () => {
     const { country_group } = useSelector(({ country }) => country);
+    const { profile } = useSelector(({ auth }) => auth);
     const [collapsed, setCollapsed] = useState(false);
 
     const Flag = styled("img")`
@@ -129,65 +166,143 @@ const SidebarLayoutComponents = () => {
     }
 
     return (
-        <Sidebar width={350} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} >
-            <div style={{ margin: "15px 10px 15px 10px" }} >
-                <section>
-                    <Logo />
-                    {!collapsed ?
-                        <NameUser>
-                            <div>น.อ.วรุณ วีระกุล</div>
-                            <div>ผชท.ทร.ไทย/ฮานอย</div>
-                        </NameUser>
-                        : null
-                    }
-                </section>
-                <div style={{
-                    overflow: 'auto',
-                    height: '70vh',
-                }}>
-                    <H1>เลือกประเทศ</H1>
-                    <Row>
-                        <ColText span={8}>{findCountryGroup("อาเซียน")?.name}</ColText>
-                        <ColText span={8}>{findCountryGroup("อาเซียน+9")?.name}</ColText>
-                        <ColText span={8}>{findCountryGroup("อื่นๆ")?.name}</ColText>
-                    </Row>
-                    <Line />
-                    <Row>
-                        <ColText span={8}>
-                            {findCountryGroup("อาเซียน")?.countries.map((e: any) => (
-                                <MenuFlag key={e.id}>
-                                    <Row>
-                                        <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
-                                        <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
-                                    </Row>
-                                </MenuFlag>
-                            ))}
-                        </ColText>
-                        <ColText span={8}>
-                            {findCountryGroup("อาเซียน+9")?.countries.map((e: any) => (
-                                <MenuFlag key={e.id}>
-                                    <Row>
-                                        <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
-                                        <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
-                                    </Row>
-                                </MenuFlag>
-                            ))}
-                        </ColText>
-                        <ColText span={8}>
-                            {findCountryGroup("อื่นๆ")?.countries.map((e: any, index: number) => (
-                                <MenuFlag key={e.id} style={{ paddingBottom: 0 }}>
-                                    <Row>
-                                        <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
-                                        <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
-                                    </Row>
-                                </MenuFlag>
-                            ))}
-                        </ColText>
+        <>
+            <Sidebar width={350} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} >
+                <div style={{ margin: "15px 10px 15px 10px" }} >
+                    <section>
+                        <Logo />
+                        {!collapsed ?
+                            <NameUser>
+                                <div>{`${profile?.rank ?? ""} ${profile?.first_name ?? ""} ${profile?.last_name ?? ""}`}</div>
+                                <div>{profile?.position}</div>
+                            </NameUser>
+                            : null
+                        }
+                    </section>
+                    <div style={{
+                        overflow: 'auto',
+                        height: '70vh',
+                    }}>
+                        <H1>เลือกประเทศ</H1>
+                        <Row>
+                            <ColText span={8}>{findCountryGroup("อาเซียน")?.name}</ColText>
+                            <ColText span={8}>{findCountryGroup("อาเซียน+9")?.name}</ColText>
+                            <ColText span={8}>{findCountryGroup("อื่นๆ")?.name}</ColText>
+                        </Row>
+                        <Line />
+                        <Row>
+                            <ColText span={8}>
+                                {findCountryGroup("อาเซียน")?.countries.map((e: any) => (
+                                    <MenuFlag key={e.id}>
+                                        <Row>
+                                            <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
+                                            <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
+                                        </Row>
+                                    </MenuFlag>
+                                ))}
+                            </ColText>
+                            <ColText span={8}>
+                                {findCountryGroup("อาเซียน+9")?.countries.map((e: any) => (
+                                    <MenuFlag key={e.id}>
+                                        <Row>
+                                            <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
+                                            <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
+                                        </Row>
+                                    </MenuFlag>
+                                ))}
+                            </ColText>
+                            <ColText span={8}>
+                                {findCountryGroup("อื่นๆ")?.countries.map((e: any, index: number) => (
+                                    <MenuFlag key={e.id} style={{ paddingBottom: 0 }}>
+                                        <Row>
+                                            <ColMenuFlag span={8}><Flag width={40} src={e.icon_path} /></ColMenuFlag>
+                                            <ColMenuFlag span={14} style={{ paddingTop: 5 }}><text>{e.initials_th}</text></ColMenuFlag>
+                                        </Row>
+                                    </MenuFlag>
+                                ))}
+                            </ColText>
 
-                    </Row>
+                        </Row>
+
+                        <IonsWorkingGroups countries={findCountryGroup("IONS Working Groups")?.countries} />
+                    </div>
                 </div>
-            </div>
-        </Sidebar>
+            </Sidebar>
+        </>
+    )
+}
+
+const IonsWorkingGroups = ({ countries }: { countries: any[] }) => {
+    const [open, setOpen] = useState(false);
+    const [memberฉountries, setMemberฉountries] = useState<any[]>([]);
+    const [observerฉountries, setObserverฉountries] = useState<any[]>([]);
+
+    useEffect(() => {
+        if (countries) {
+            // 1 คือ สมาชิก 2 คือ ผู้สังเกตการณ์
+            setMemberฉountries(countries.filter(w => w.status === 1))
+            setObserverฉountries(countries.filter(w => w.status === 2))
+        }
+    }, [countries])
+
+
+    const showModal = () => {
+        setOpen(true);
+    };
+
+    const hideModal = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <a onClick={showModal}><H1 style={{ paddingTop: 10 }}><u>IONS Working Groups</u></H1></a>
+            <ModalIWG
+                bodyStyle={{
+                    maxHeight: 700,
+                    overflowY: "auto"
+                }}
+                width={"70vw"}
+                title={"IONS Working Groups"}
+                open={open}
+                onOk={hideModal}
+                onCancel={hideModal}
+                footer={null}
+            >
+                <p style={{ color: "#00408E" }}>PARTICIPATING NATIONS</p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>สมาชิก</th>
+                            <th>ผู้สังเกตการณ์</th>
+                        </tr>
+                        <tr>
+                            <td style={{ borderRight: "1px solid #dddddd" }}>
+                                <FlagIWGGroup>
+                                    {memberฉountries.map((e: any) => (
+                                        <FlagIWG key={e.icon_path}>
+                                            <img src={e.icon_path} />
+                                            <div className='text'>{e.initials_th}</div>
+                                        </FlagIWG>
+                                    ))}
+                                </FlagIWGGroup>
+                            </td>
+                            <td style={{ display: "flex" }}>
+                                <FlagIWGGroup>
+                                    {observerฉountries.map((e: any) => (
+                                        <FlagIWG key={e.icon_path}>
+                                            <img src={e.icon_path} />
+                                            <div className='text'>{e.initials_th}</div>
+                                        </FlagIWG>
+                                    ))}
+                                </FlagIWGGroup>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </ModalIWG>
+        </>
     )
 }
 
